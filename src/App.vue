2fa69f63a45fc1108c97f3d12756095055eaa277
chill <dimiefuchen@qq.com>
2024-10-24 19:05:04 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <div class="header">
+      <div class="box">
+        <h1>
+          <img src="../static/yln.png" alt="404" width="56" height="45" />
+          <span class="title">App Store</span>
+        </h1>
+        <nav class="tag">
+          <router-link to="/">商店首页</router-link>
+        </nav>
+      </div>
+      <div class="box">
+        <input class="text" type="text" name="keyword" placeholder="请输入你想要的商品">
+        <input class="button" type="button" value="搜索">
+        <nav class="tag">
+          <router-link to="/login}">Login</router-link>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,12 +28,37 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 60px;
+  background-color: lightpink;
+  color: lightskyblue;
+}
+.box {
+  display: flex;
+  align-items: center;
+}
+.tag {
+  color: lightskyblue;
+}
+.text {
+  height: 22px;
+  font-size: 14px;
+  border: 1px solid lavender;
+  padding: 3px 16px;
+  border-bottom-left-radius: 20px;
+  border-top-left-radius: 20px;
+}
+.button{
+  width: 60px;
+  height: 30px;
+  font-size: 14px;
+  margin-right: 35px;
+  border: 1px solid lavender;
+  background-color: lightpink;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import head from '../App.vue'
+import login from '../components/Login.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'head',
+      component: head
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
     }
   ]
 })
